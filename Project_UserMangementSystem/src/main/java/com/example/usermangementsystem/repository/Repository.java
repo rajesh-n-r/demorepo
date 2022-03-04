7a32fae1d583ca7e3dcabcd3fa4a87adcc6c838d
rajesh-n-r <56992379+rajesh-n-r@users.noreply.github.com>
2022-03-04 16:12:34 +0530

    Delete Project_UserMangementSystem directory

--- Project_UserMangementSystem/src/main/java/com/example/usermangementsystem/repository/Repository.java
+++
@@ -1,7 +0,0 @@
-package com.example.usermangementsystem.repository;
-
-import com.example.usermangementsystem.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<User, Integer> {
-}
